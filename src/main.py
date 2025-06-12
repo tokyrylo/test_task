7d23480bd3da96cfb3537bf6dad5b4549a2def49
To Kirill <tio.kirill@gmail.com>
2025-06-12 19:55:36 +0300

    fix root router

--- src/main.py
+++ src/main.py
@@ -5,9 +5,9 @@ from src.booking.routes.router import router as booking_router
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
 
 
 app.include_router(booking_router)
